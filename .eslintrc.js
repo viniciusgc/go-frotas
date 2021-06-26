1ad0db568a8a89e8a62daf569e15d3f0144fcd7c
ViniciusExacta <vinicius.carvalho@exactaworks.com.br>
2021-06-26 09:46:37 -0300

    feat: first commit

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: ["airbnb", "prettier", "prettier/react"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "prettier", "react-hooks"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".jsx", "js"]
+      }
+    ],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-no-undef": "off",
+    "import/prefer-default-export": "off",
+    "no-console": [
+      "warn",
+      {
+        allow: ["error"]
+      }
+    ],
+    "no-unused-expressions": [
+      "off",
+      {
+        extensions: [".jsx"]
+      }
+    ],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-props-no-spreading": "off",
+    "react/prop-types": [0, { ignore: ["children"] }],
+    "react/destructuring-assignment": "off"
+  },
+  settings: {
+    "import/core-modules": []
+  }
+};
